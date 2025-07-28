6804196088da9b69b2630a15f51edba59d7a3000
ErikaArci99 <erikaarci1999@outlook.it>
2025-07-28 15:21:14 +0200

    Add constructor to Studente class with parameters for nome, cognome, and eta

--- src/org/lessons/java/snack/Studente.java
+++ src/org/lessons/java/snack/Studente.java
@@ -10,4 +10,10 @@ public class Studente {
     private String cognome;
     private int eta;
 
+    // Costruttore che accetta tre parametri
+    public Studente(String nome, String cognome, int eta) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+    }
 }
