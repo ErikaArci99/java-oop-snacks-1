bfb1c90f96f188daeae6a9cee5386faa2ffd0c43
ErikaArci99 <erikaarci1999@outlook.it>
2025-07-28 15:19:09 +0200

    Add initial implementation of App and Studente classes

---
+++ src/org/lessons/java/snack/Studente.java
@@ -0,0 +1,13 @@
+// Snack 1: Studente
+// Scrivi una classe Studente con i seguenti attributi: nome, cognome, età. Implementa un costruttore che accetta tre parametri per inizializzare questi attributi. Aggiungi un metodo che restituisca una stringa concatenata dei valori (es. Anselmo Bitta, 24 anni).
+
+package org.lessons.java.snack;
+
+public class Studente {
+
+    // Attributi della classe Studente
+    private String nome;
+    private String cognome;
+    private int eta;
+
+}
