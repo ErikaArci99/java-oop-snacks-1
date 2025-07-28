aa80b700aa54a854d677ca937c65d3240ba9329a
ErikaArci99 <erikaarci1999@outlook.it>
2025-07-28 15:23:02 +0200

    Add ContoBancario class with attributes for account number and balance

---
+++ src/org/lessons/java/snack/ContoBancario.java
@@ -0,0 +1,11 @@
+// Snack 2: Gestione di un Conto
+// Crea una classe ContoBancario con attributi per numero di conto e saldo. Implementa un costruttore che accetta il numero di conto e inizializza il saldo a zero. Aggiungi metodi pubblici per depositare denaro sul conto, prelevare denaro dal conto e ottenere il saldo corrente.
+
+package org.lessons.java.snack;
+
+public class ContoBancario {
+
+    // Attributi della classe ContoBancario
+    private String numeroConto;
+    private double saldo;
+}
